d15d4dab14e8b4b0dcc91be42ce66ee0c58e840b
vicksen <lichengjun0806@outlook.com>
2025-11-13 19:25:03 -0300

    Initial Commit

---
+++ OfflineSocialApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OfflineSocialApp
+//
+//  Created by Nao Tof on 11/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OfflineSocialApp/OfflineSocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfflineSocialAppApp.swift
+//  OfflineSocialApp
+//
+//  Created by Nao Tof on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct OfflineSocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
